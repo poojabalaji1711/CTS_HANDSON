0556fdecce04fef08ccec0ff8df2e215af85f38a
Pooja B <pooja17112005@gmail.com>
2026-06-23 12:23:32 +0530

    Added Mockito Exercise 2 - Verifying Interactions

---
+++ Week1_Deepskilling/TDD using JUnit5 and Mockito/3. Mockito exercises/VerifyingInteractions/src/test/java/com/example/VerifyingInteractionsTest.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class VerifyingInteractionsTest {
+
+    @Test
+    public void testVerifyInteraction() {
+
+        
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+
+     
+        MyService service = new MyService(mockApi);
+
+        
+        service.fetchData();
+
+       
+        verify(mockApi).getData();
+    }
+}
