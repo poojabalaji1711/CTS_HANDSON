e66ed2667c781ba875855d1ccf5255cadcf86bd6
Pooja B <pooja17112005@gmail.com>
2026-07-15 19:11:22 +0530

    Task completed

--- Week5_Deepskilling/09_ReactJS-HOL/cricketapp/src/Components/ListofPlayers.js
+++ Week5_Deepskilling/09_ReactJS-HOL/cricketapp/src/Components/ListofPlayers.js
@@ -1,52 +1,41 @@
+import { OddPlayers, EvenPlayers, IndianPlayers } from "./IndianPlayers";
+import ListofIndianPlayers from "./ListofIndianPlayers";
 
-import { OddPlayers, EvenPlayers, IndianPlayers } from "./Components/IndianPlayers";
-import ListofIndianPlayers from "./Components/ListofIndianPlayers";
-
-function ListofPlayers() {
-  const flag = true;
-
-  const players = [
-    { name: "Jack", score: 50 },
-    { name: "Michael", score: 70 },
-    { name: "John", score: 40 },
-    { name: "Ann", score: 61 },
-    { name: "Elisabeth", score: 61 },
-    { name: "Sachin", score: 95 },
-    { name: "Dhoni", score: 100 },
-    { name: "Virat", score: 84 },
-    { name: "Jadeja", score: 64 },
-    { name: "Raina", score: 75 },
-    { name: "Rohit", score: 80 },
-  ];
-
-  if (flag) {
+function ListofPlayers({ players }) {
+  if (players) {
     return (
-      <div>
-        <h1>List of Players</h1>
-        <ListofPlayers players={players} />
-        <hr />
-        <h1>List of Players having Scores Less than 70</h1>
-        <ListofPlayers players={players.filter((p) => p.score <= 70)} />
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>Odd Players</h1>
-        <OddPlayers />
-
-        <hr />
-
-        <h1>Even Players</h1>
-        <EvenPlayers />
-
-        <hr />
-
-        <h1>List of Indian Players Merged</h1>
-        <ListofIndianPlayers IndianPlayers={IndianPlayers} />
-      </div>
+      <ul>
+        {players.map((player, index) => (
+          <li key={index}>
+            {player.name} - {player.score}
+          </li>
+        ))}
+      </ul>
     );
   }
+
+  const flag = false;
+
+  if (flag) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h1>Odd Players</h1>
+      <OddPlayers />
+
+      <hr />
+
+      <h1>Even Players</h1>
+      <EvenPlayers />
+
+      <hr />
+
+      <h1>List of Indian Players Merged</h1>
+      <ListofIndianPlayers IndianPlayers={IndianPlayers} />
+    </div>
+  );
 }
 
 export default ListofPlayers;
